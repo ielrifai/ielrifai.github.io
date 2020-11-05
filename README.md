8392e23db117839db181ccfc1fdcaf6d625dc3c0
Issa <ielrifai@uwo.ca>
2020-11-05 00:24:47 -0500

    Updates

--- README.md
+++
@@ -1,9 +0,0 @@
-# PORTFOLIO_Website.v2
-
-This is the implementaion code for the website
-- To run this project follow these steps , 
-  - clone the repo using `git clone`
-  - navigate to folder `portfolio_website`
-  - run `npm install` to instal node modules
-  - run `npm start` to start the service on `localhost:3000`
-    
---
+++ precache-manifest.7ebead13e303ca8f331c2acbbec1ac1e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "388e1063029b95c99249",
+    "url": "/Portfolio-Website.v2/static/css/main.96882f83.chunk.css"
+  },
+  {
+    "revision": "388e1063029b95c99249",
+    "url": "/Portfolio-Website.v2/static/js/main.388e1063.chunk.js"
+  },
+  {
+    "revision": "3fa8e3ddc056a61fe9c7",
+    "url": "/Portfolio-Website.v2/static/js/runtime~main.3fa8e3dd.js"
+  },
+  {
+    "revision": "c2af88fd30d232974907",
+    "url": "/Portfolio-Website.v2/static/js/2.c2af88fd.chunk.js"
+  },
+  {
+    "revision": "7e743575bb17ae58711c50896cab70a1",
+    "url": "/Portfolio-Website.v2/index.html"
+  }
+];
